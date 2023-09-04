9e61be91ef0140e0cf123a2029c6bbbee798fa17
m1gu3l23 <143034758+m1gu3l23@users.noreply.github.com>
2023-09-03 22:21:24 -0300

    Update listaExercicios01.sql

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -1,3 +1,4 @@
 select titulo from livros;
 select nome from autores where nascimento < '1900-01-01'; 
 select livros.titulo from livros inner join autores on autores.nome = 'J.K. Rowling';
+select alunos.nome from alunos inner join  matriculas on matriculas.curso = 'Engenharia de software' and matriculas.aluno_id = aluno.id;
