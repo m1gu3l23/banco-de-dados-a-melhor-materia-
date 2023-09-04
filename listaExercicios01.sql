059d84f0ccf305b01e480cf2b902d369b144e79c
m1gu3l23 <143034758+m1gu3l23@users.noreply.github.com>
2023-09-03 22:42:27 -0300

    Update listaExercicios01.sql

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -3,6 +3,6 @@ select nome from autores where nascimento < '1900-01-01';
 select livros.titulo from livros inner join autores on autores.nome = 'J.K. Rowling';
 select alunos.nome from alunos inner join  matriculas on matriculas.curso = 'Engenharia de software' and matriculas.aluno_id = aluno.id;
 select produto, avg(receita) receita_total from vendas group by produto;
-select autores.nome, count(livros.id) livros_total from autores left join livros on autores.id = livros.autor_id group by autores.nome;
-select curso, count(aluno_id) qt_alunos from matriculas group by curso;
+select autores.nome, count(livros.id) a livros_total from autores left join livros on autores.id = livros.autor_id group by autores.nome;
+select curso, count(aluno_id) a qt_alunos from matriculas group by curso;
 select produto, avg(receita) média_receita from vendas group by produto
